c86ceaddef78907aee1b83eb9fdc4a1f2672b990
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-12-24 14:20:50 +0300

    Interface service is created for model objects.

---
+++ Northwind.InterfaceLayer/Abstract/ModelService/ICategorySalesFor1997Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.InterfaceLayer.Abstract.ModelService
+{
+    public interface ICategorySalesFor1997Service
+    {
+    }
+}
