fe354021e21508142fb2119130f116d02279ffb2
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-01-16 17:14:03 +0300

    Access modifier is written.

--- Northwind.BusinessLogicLayer/Abstract/IClass.cs
+++ Northwind.BusinessLogicLayer/Abstract/IClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Northwind.BusinessLogicLayer.Abstract
 {
-    class IClass
+    internal interface IClass
     {
     }
 }
--- Northwind.InterfaceLayer/Concrete/Class.cs
+++ Northwind.InterfaceLayer/Concrete/Class.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Northwind.InterfaceLayer.Concrete
 {
-    class Class
+    internal class Class
     {
     }
 }
